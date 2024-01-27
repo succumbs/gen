234faf58cb682ccd3539c069d512f0d53a8f908f
june <june0925@proton.me>
2024-01-26 21:58:58 -0500

    add deps

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,12 @@ mod short_wordlist;
 
 use clap::{Parser, Subcommand, ValueEnum};
 
-
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
-
 
 #[derive(Subcommand)]
 enum Commands {
@@ -19,17 +17,16 @@ enum Commands {
         /// length of the passphrase (in words)
         #[arg(short, long, default_value_t = 6)]
         length: u8,
-       
+
         /// separator between words
         #[arg(short, long, default_value = "-")]
         separator: String,
-        
+
         /// specify which word list to use
         #[arg(long, value_enum, default_value_t = WordList::Long)]
         list: WordList,
     },
 }
-
 
 #[derive(Clone, ValueEnum)]
 enum WordList {
