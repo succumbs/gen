1b699be537551edf5a6b2d42024538e94c0bac68
june <june0925@proton.me>
2024-01-26 21:35:09 -0500

    setup for passphrase command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+
+#[derive(Subcommand)]
+enum Commands {
+    /// generate a passphrase
+    Passphrase {
+        /// length of the passphrase (in words)
+        #[arg(short, long, default_value_t = 6)]
+        length: u32,
+       
+        /// separator between words
+        #[arg(short, long, default_value = "-")]
+        separator: String,
+        
+        /// specify which word list to use
+        #[arg(long, value_enum, default_value_t = WordList::Long)]
+        list: WordList,
+    },
+}
+
+
+#[derive(Clone, ValueEnum)]
+enum WordList {
+    /// use the long word list
+    Long,
+    /// use the short word list
+    Short,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
